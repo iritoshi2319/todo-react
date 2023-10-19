f868b2b031d6b77485013cc75e6aea71a0d9c5e2
Toshihiko Irishio <72386294+iritoshi2319@users.noreply.github.com>
2023-10-19 16:51:21 +0000

    TODO：HTML

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <React.Fragment>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li className="list-row">
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li className="list-row">
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li className="list-row">
+            <p>TODOです</p>
+            <button>戻す</button>
+          </li>
+          <li className="list-row">
+            <p>TODOです</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
