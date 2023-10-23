93212d356d87da983b855c43d5303e126ed105b3
Toshihiko Irishio <72386294+iritoshi2319@users.noreply.github.com>
2023-10-23 10:05:55 +0000

    TODO：未完了リストの削除処理実装

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,12 @@ export const App = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <React.Fragment>
       <div className="input-area">
@@ -37,12 +43,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <li key={todo} className="list-row">
                 <p>{todo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </li>
             );
           })}
